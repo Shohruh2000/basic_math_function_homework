33578cad76fbb15897296fbefa5a2cb2870d668a
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 06:26:40 +0500

    misol

--- basic_func11.py
+++ basic_func11.py
@@ -1,8 +1,8 @@
 # Create a function called main.
 # Create function argument a.
 # Return the absolute value of a.
+import math 
 def main(a):
-    s = -1
-    s *=a
-    return s
-print(main(-1))
+    return a
+y = main(abs(-7))
+print(y)
