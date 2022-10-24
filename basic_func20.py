092b7016e9dbd6f71e8b6064cb2a7df349b26063
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-24 07:31:02 +0500

    misol

--- basic_func20.py
+++ basic_func20.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
+import math
+
+def main(a,b):
+    return a*b
+y = main(12,13)
+print(y)
